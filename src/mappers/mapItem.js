253fa5f00171bcfd39ed8b7ae3088eb4cdb9960a
Yaniv Solnik <solnik@gmail.com>
2019-06-25 15:49:46 +0300

    changed duration and added video play link

--- src/mappers/mapItem.js
+++ src/mappers/mapItem.js
@@ -2,10 +2,10 @@ import {
   getArrayFieldValue,
   createMediaGroupItem,
   getVideoSource
-} from '../utils';
-import moment from 'moment';
+} from "../utils";
+import moment from "moment";
 
-export function mapItem(auth_id, image_key = 'image_base', free = true) {
+export function mapItem(auth_id, image_key = "image_base", free = true) {
   return item => {
     try {
       const {
@@ -19,18 +19,18 @@ export function mapItem(auth_id, image_key = 'image_base', free = true) {
       const published = moment(
         new Date(getArrayFieldValue(_published))
       ).format();
-      
+
       const id = getArrayFieldValue(guid, true)._;
       const title = getArrayFieldValue(_title);
       const summary = getArrayFieldValue(_summary);
 
-      const images = item['media:thumbnail'][0];
+      const images = item["media:thumbnail"][0];
 
       const imageKeys = [`url:${image_key}`];
       let media_group = imageKeys
         .map(imageKey => {
           try {
-            const arr = imageKey.split(':');
+            const arr = imageKey.split(":");
             if (images.$[arr[0]]) {
               return createMediaGroupItem(images.$[arr[0]], arr[1]);
             }
@@ -40,34 +40,23 @@ export function mapItem(auth_id, image_key = 'image_base', free = true) {
         })
         .filter(i => i);
 
-      let link;
-      if (_link && getArrayFieldValue(_link)) {
-        link = {
-          type: 'text/html',
-          rel: 'alternate',
-          href: getArrayFieldValue(_link)
-        };
-      }
+      const link = {
+        type: "text/html",
+        rel: "alternate",
+        href: `hearst://plugin?type=player&action=playVodItem&vodItemId=${id}`
+      };
 
-      const videos = item['media:group'][0]['media:content'].map(
+      const videos = item["media:group"][0]["media:content"].map(
         video => video.$
       );
-      const {src, duration: _duration} = getVideoSource(videos);
-      const content = { src, type: 'video/hls' };
-
-      let duration;
-      try {
-        duration = `${Math.round(parseFloat(_duration) / 60).toString().padStart(2, '0')}min`;
-      } catch (err) {
-
-      }
-
+      const { src, duration } = getVideoSource(videos);
+      const content = { src, type: "video/hls" };
       const videoAds = [];
       const extensions = { free, auth_id, videoAds, duration };
 
       return {
         type: {
-          value: 'video'
+          value: "video"
         },
         id,
         title,
