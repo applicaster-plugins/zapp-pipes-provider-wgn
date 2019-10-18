9e0e14c48fb266af5464da67c40ab8afe1e79e6b
hannypeleg <h.peleg@applicaster.com>
2019-10-18 14:34:35 -0400

    present top-level shows (#4)

--- src/api/getAllSeries.js
+++ src/api/getAllSeries.js
@@ -3,7 +3,7 @@ import { config } from '../config';
 
 export async function getAllSeries() {
   try {
-    const response = await axios.get(`${config.api.baseUrl}/wgn-series`);
+    const response = await axios.get(`${config.api.baseUrl}/wgn-series?parent=0`);
     return response.data;
   } catch (err) {}
 }
