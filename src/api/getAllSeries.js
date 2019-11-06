20595e70a3be5bd62a0f73421e414374983bf2a6
eladkariti2 <e.kariti@applicaster.com>
2019-11-05 20:22:51 -0500

    support 100 items per request (#14)

--- src/api/getAllSeries.js
+++ src/api/getAllSeries.js
@@ -3,7 +3,7 @@ import { config } from '../config';
 
 export async function getAllSeries() {
   try {
-    const response = await axios.get(`${config.api.baseUrl}/wgn-series?parent=0`);
+    const response = await axios.get(`${config.api.baseUrl}/wgn-series?parent=0&per_page=100`);
     return response.data;
   } catch (err) {}
 }
--- src/api/getAllVideos.js
+++ src/api/getAllVideos.js
@@ -3,7 +3,7 @@ import { config } from '../config';
 
 export async function getAllVideos() {
   try {
-    const response = await axios.get(`${config.api.baseUrl}/wgn-videos`);
+    const response = await axios.get(`${config.api.baseUrl}/wgn-videos?per_page=100`);
     return response.data;
   } catch (err) {}
 }
