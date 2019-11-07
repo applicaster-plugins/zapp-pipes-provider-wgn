271445b9c93663bfd8e17c5be01e10f91a9a84cb
Elad Kariti <e.kariti@applicaster.com>
2019-11-07 10:06:19 -0500

    delete logs

--- src/api/getItemsByTag.js
+++ src/api/getItemsByTag.js
@@ -5,12 +5,9 @@ import { getTagdId } from '../utils/index'
  
 export async function getItemsByTag(tag, routType) {
   try {
-    console.log("routType: " + routType);
-    
     var tagId = await getTagdId(tag);
     var route = "wgn-videos" // default route
     route = routType ?  config.tagMap[routType] : route;
-    console.log("route: " + route);
     const response = await axios.get(
       `${config.api.baseUrl}/${route}/?tags=${tagId}&per_page=100`
     );
